5ce5a3179dd0b73622ff7b150b84e345999f931c
ATreTCM <atret1988@gmail.com>
2023-01-23 20:34:06 +0200

    added host

--- numiralogy/settings.py
+++ numiralogy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = token
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'netlify.app']
 
 
 # Application definition
